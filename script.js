db8058f6672a8fb98b9d2284902d2224781dbd38
Guilherme Lima <guilhermelimaesilva.p@gmail.com>
2025-09-26 23:00:52 -0300

    add: Adicionando script e função de dark e light mode

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+
+    const img = document.querySelector('#profile img')
+    if (html.classList.contains('light')) {
+        return img.setAttribute('src', './Assets/avatar-light.png')
+    } else {
+        img.setAttribute('src', "./Assets/avatar.png")
+    }
+
+
+    // if (html.classList.contains('light')) {
+    //     html.classList.remove('light')
+    // } else {
+    //     html.classList.add('light')
+    // }
+    
+
+    
+}
+
